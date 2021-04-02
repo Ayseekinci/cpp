fba3cd9508ad649589d2844719807cb81489c4b6
Ayşe <Ayseekinci.02.00@gmail.com>
2021-04-02 12:28:36 +0300

    Create donguler2.cpp

---
+++ donguler2.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+
+using namespace std;
+
+//Girilen 4 notun ortalamasi
+
+/*int main(){
+	int toplam,nott,ortalama=0,sayac=1;
+	
+	while(sayac<=4){
+		cout<<"Not :";
+		cin>>nott;
+		
+		toplam +=nott;
+		sayac++;
+	}
+	cout<<"Toplam : "<<toplam<<endl;
+	ortalama=toplam/4;
+	cout<<"Ortalama:"<<ortalama;
+}
+*/
+
+
+
+//Faktöriyel Hesaplama
+
+/*int main(){
+	int sayi,x=1,faktoriyel;
+	cout<<"Faktoriyelini almak istedigiz sayi : ";
+	cin>>sayi;
+	
+	while(x<sayi){
+		x++;
+		faktoriyel=faktoriyel*x;
+		
+	}
+	cout<<"Faktoriyel: "<<faktoriyel;
+}*/
+
+
+
+
+//1’den n’e kadar olan tek ve çift sayýlarýn toplamýný bulan program
+
+/*int main(){
+	int sayi,toplamtek=0,toplamcift=0;
+	cout<<"Sayi: ";
+	cin>>sayi;
+	
+	do{
+		if(sayi%2==0){
+			toplamcift+=sayi;
+		}
+		else if(sayi%2!=0){
+			toplamtek+=sayi;
+		}
+		sayi--;
+		
+	}while(sayi>0);
+	cout<<"Toplam Cift= "<<toplamcift<<endl;
+	cout<<"Toplam Tek= "<<toplamtek;
+}*/
+
+
+
+//For Pramit Ornegi
+
+/*int main(){
+	for(int i=0;i<3;i++){
+		for(int bosluk=0;bosluk<3-(i+1);bosluk++){
+			cout<<" ";
+		}
+		for(int j=0;j<=i*2;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	
+	}
+	}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
